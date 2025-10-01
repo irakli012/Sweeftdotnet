e0c7cde23ee2ca4a3063ab3422e05a450c767dd5
Iraklimate <iraklimate@gmail.com>
2025-10-02 01:27:25 +0400

    add task 1 - palindrome

---
+++ SweeftTasks/SweeftTasks/Algorithms/Palindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweeftTasks.Algorithms
+{
+    public static class Palindrome
+    {
+        public static bool IsPalindrome(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text)) return false;
+
+            int i = 0;
+            int j = text.Length - 1;
+
+            while (i < j)
+            {
+                if (text[i] != text[j])
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
--- SweeftTasks/SweeftTasks/Program.cs
+++ SweeftTasks/SweeftTasks/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SweeftTasks.Algorithms;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Task 1 --- Palindrome");
+        Console.WriteLine(Palindrome.IsPalindrome("car"));
+        Console.WriteLine(Palindrome.IsPalindrome("abcba"));
+
+    }
+}

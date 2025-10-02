827413664386cb9331e98edfb682fa22332a2f39
Iraklimate <iraklimate@gmail.com>
2025-10-03 03:02:34 +0400

    add pupil&teacher models for task 7

---
+++ SweeftTasks/SweeftTasks/Models/Pupil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweeftTasks.Models
+{
+    public class Pupil
+    {
+        public int PupilId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Gender { get; set; }
+        public string? Class { get; set; }
+
+        public ICollection<Teacher> Teachers { get; set; } = new List<Teacher>();
+    }
+}
---
+++ SweeftTasks/SweeftTasks/Models/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweeftTasks.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Gender { get; set; }
+        public string? Subject { get; set; }
+
+        public ICollection<Pupil> Pupils { get; set; } = new List<Pupil>();
+    }
+}

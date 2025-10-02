e21a34bc6f87e466181b60523e325e8a0f0d354d
Iraklimate <iraklimate@gmail.com>
2025-10-03 03:07:02 +0400

    add seeddata for testing

---
+++ SweeftTasks/SweeftTasks/Data/SeedData.cs
@@ -0,0 +1,34 @@
+﻿using SweeftTasks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweeftTasks.Data
+{
+    public static class SeedData
+    {
+        public static void Seed(SchoolDbContext context)
+        {
+            if (context.Teachers.Any())
+                return;
+
+            var teacher1 = new Teacher { FirstName = "John", LastName = "smith", Subject = "Math" };
+            var teacher2 = new Teacher { FirstName = "walter", LastName = "white", Subject = "Science" };
+
+            var pupil1 = new Pupil { FirstName = "Jesse", LastName = "Pinkman" };
+            var pupil2 = new Pupil { FirstName = "Giorgi", LastName = "Johnson" };
+            var pupil3 = new Pupil { FirstName = "Johnny", LastName = "White" };
+
+            teacher1.Pupils.Add(pupil1);
+            teacher2.Pupils.Add(pupil1);
+            teacher2.Pupils.Add(pupil2);
+            teacher1.Pupils.Add(pupil3);
+
+            context.Teachers.AddRange(teacher1, teacher2);
+
+            context.SaveChanges();
+        }
+    }
+}
